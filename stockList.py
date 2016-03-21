dbe5a2093c820282afe50767cdfba751a776ca82
Tom Swanson <tswanson@osii.com>
2016-03-21 09:09:12 -0500

    Fetch NASDAQ file. Delete pyc file from repository.

--- stockList.py
+++ stockList.py
@@ -1,13 +1,22 @@
+from ftplib import FTP
 
 # Download the NASDAQ's list of stock symbols
 def fetchNASDAQfile():
-    print "Fetching NASDAQ file."
-
+    print "Fetching NASDAQ file..."
+    try:
+        ftp = FTP('ftp.nasdaqtrader.com')
+        ftp.login()
+        ftp.cwd('SymbolDirectory')
+        ftp.retrbinary('RETR nasdaqlisted.txt', open('nasdaqlisted.txt', 'wb').write)
+        print("Done fetching NASDAQ file.")
+    except:
+        print("Error! Could not fetch list of NASDAQ stock symbols.")
 
 # Reads the NASDAQ file, parses it, and returns a list of all stock symbols
 def parseNASDAQfile():
-    print "Parsing NASDAQ file."
+    print "Parsing NASDAQ file..."
 
+    print("Done parsing NASDAQ file.")
 
 # Returns a list of all stock symbols
 def getStockList():
