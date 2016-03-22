f6394fbf8cffbfba08e0247ee5b1f22da004cbef
Tom Swanson <trswany@gmail.com>
2016-03-22 08:56:04 -0500

    Loop the program and delay 2 hours instead of 30 seconds.

--- topStocks.py
+++ topStocks.py
@@ -6,37 +6,39 @@ from stockList import getStockList
 import time
 
 def main():
-    # Get the list of stock symobls
-    currentStockList = getStockList()
 
-    # Get the stock prices
-    oldStockPrices = stockPrices.getStockPrices(currentStockList)
+    while 1:
+        # Get the list of stock symobls
+        currentStockList = getStockList()
 
-    # Wait a while for the stock prices to change
-    print("Sleeping...")
-    time.sleep(30)
-    print("Done sleeping.")
+        # Get the stock prices
+        oldStockPrices = stockPrices.getStockPrices(currentStockList)
 
-    # Get the new stock prices
-    currentStockPrices = stockPrices.getStockPrices(currentStockList)
+        # Wait a while for the stock prices to change
+        print("Sleeping...")
+        time.sleep(7200)
+        print("Done sleeping.")
 
-    # Find the fastest-changing stocks
-    topStockPrices = stockPrices.getFastestChangingPrices(currentStockPrices, oldStockPrices)
+        # Get the new stock prices
+        currentStockPrices = stockPrices.getStockPrices(currentStockList)
 
-    # Format the tweet text
-    tweetText = ""
-    for stockPrice in topStockPrices:
-        symbol  = stockPrice[0]
-        percentage = round(float(stockPrice[1]), 2)
-        if percentage > 0:
-            percentage = '+' + str(percentage)
-        else:
-            percentage = str(percentage)
-        tweetText = tweetText + '#' + symbol + " : " + percentage + '%, '
+        # Find the fastest-changing stocks
+        topStockPrices = stockPrices.getFastestChangingPrices(currentStockPrices, oldStockPrices)
 
-    # Post a tweet of the top stocks
-    print tweetText
-    tweetPoster.postTweet(tweetText)
+        # Format the tweet text
+        tweetText = ""
+        for stockPrice in topStockPrices:
+            symbol  = stockPrice[0]
+            percentage = round(float(stockPrice[1]), 2)
+            if percentage > 0:
+                percentage = '+' + str(percentage)
+            else:
+                percentage = str(percentage)
+            tweetText = tweetText + '#' + symbol + " : " + percentage + '%, '
+
+        # Post a tweet of the top stocks
+        print tweetText
+        tweetPoster.postTweet(tweetText)
 
 if __name__ == '__main__':
     sys.exit(main())
